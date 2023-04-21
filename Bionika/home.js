f47d3b10fe65631828176081be095f068ec83d15
micha <michaldrozd.u@gmail.com>
2023-04-21 19:47:37 +0200

    First

---
+++ Bionika/home.js
@@ -0,0 +1,14 @@
+function home()
+{
+    var navLinks = document.getElementById("navLinks");
+    function showMenu()
+    {
+        navLinks.style.right = "0";
+    }
+    function hideMenu()
+    {
+        navLinks.style.right = "-200px";
+    }
+}
+
+        
